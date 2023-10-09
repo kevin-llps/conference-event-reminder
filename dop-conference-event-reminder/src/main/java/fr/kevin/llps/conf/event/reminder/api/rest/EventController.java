2bf0445c7216609ef96f9172eb3518bf626a7a00
kevin-llps <llopis.kevin@outlook.fr>
2023-10-09 15:07:22 +0200

    feat: Migrate to compatible lombok version with Java 21 + update spring-boot-starter-parent

--- dop-conference-event-reminder/src/main/java/fr/kevin/llps/conf/event/reminder/api/rest/EventController.java
+++ dop-conference-event-reminder/src/main/java/fr/kevin/llps/conf/event/reminder/api/rest/EventController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
